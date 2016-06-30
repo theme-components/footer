a64ae6062e424fd318c3e6e52747228bdc697e3c
Duoc Nguyen <nguyenvanduocit@gmail.com>
2016-06-30 23:49:35 +0700

    Add footer 3 and update package info

---
+++ src/components/footer3/Index.vue
@@ -0,0 +1,15 @@
+<template>
+  <footer class="footer">
+
+  </footer>
+</template>
+
+<script>
+  import 'bootstrap/scss/bootstrap-flex.scss'
+  export default {}
+</script>
+
+<style lang="scss" scoped>
+  footer {
+  }
+</style>
