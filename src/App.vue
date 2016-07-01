7e8de3e54e38b6df710ed753aee71761d66ca245
Duoc Nguyen <nguyenvanduocit@gmail.com>
2016-07-01 14:17:56 +0700

    merge footer laracasts

--- src/App.vue
+++ src/App.vue
@@ -16,16 +16,15 @@ import Footer2 from './components/footer2/Index.vue'
 import Footer3 from './components/footer3/Index.vue'
 import Footer4 from './components/footer4/Index.vue'
 import Footer5 from './components/footer5/Index.vue'
-import 'bootstrap/scss/bootstrap-reboot.scss'
 import FooterLaracast from './components/footer-laracast/Index.vue'
+import 'bootstrap/scss/bootstrap-reboot.scss'
 
 export default {
   components: {
     'footer-1': Footer1,
     'footer-2': Footer2,
     'footer-3': Footer3,
-    'footer-4': Footer4,
-    'footer-laracast': FooterLaracast
+    'footer-laracast': FooterLaracast,
     'footer-4': Footer4,
     'footer-5': Footer5
   }
